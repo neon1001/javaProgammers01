b19594807f2e26f47f20edae77007e358cf29798
sihyun <sihyun6236@gmail.com>
2025-11-20 11:53:52 +0900

    문자열의 앞의 n글자

---
+++ src/programmers/lv0/P181907.java
@@ -0,0 +1,9 @@
+package programmers.lv0;
+
+public class P181907 {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        answer = answer + my_string.substring(0, n);
+        return answer;
+    }
+}
